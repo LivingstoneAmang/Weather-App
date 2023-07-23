22b1f2b7d75be910ff10d9aef96324bc981c56db
LivingstoneAmang <kenstone211@gmail.com>
2023-07-23 20:27:40 +0200

    committedmain branch

---
+++ script.js
@@ -0,0 +1,40 @@
+let country = document.querySelector(".country");
+const weatherIcon = document.querySelector(".weather-icon");
+const apiKey = "9db748e6e07d38a07eb023ecfe437a9a";
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+async function checkWeather(city) {
+  const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    let temperature = data.main.temp;
+    document.querySelector(".temp").innerHTML = Math.round(temperature) + "°C";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " " + "km";
+    country.innerHTML = data.sys.country;
+    if (data.weather[0].main === "clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main === "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main === "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main === "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main === "Mist") {
+      weatherIcon.src = "images/mist.png";
+    }
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
